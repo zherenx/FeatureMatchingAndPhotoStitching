8cdb544aac7ed2f067e478f6c6e78d6c134eed16
Justin Xiao <jx16dev@gmail.com>
2018-07-21 15:00:11 -0700

    Add data file.

---
+++ code/get_interest_points.m
@@ -0,0 +1,40 @@
+% Local Feature Stencil Code
+% CS 4476 / 6476: Computer Vision, Georgia Tech
+% Written by James Hays
+
+% Returns a set of interest points for the input image
+
+% 'image' can be grayscale or color, your choice.
+% 'feature_width', in pixels, is the local feature width. It might be
+%   useful in this function in order to (a) suppress boundary interest
+%   points (where a feature wouldn't fit entirely in the image, anyway)
+%   or (b) scale the image filters being used. Or you can ignore it.
+
+% 'x' and 'y' are nx1 vectors of x and y coordinates of interest points.
+% 'confidence' is an nx1 vector indicating the strength of the interest
+%   point. You might use this later or not.
+% 'scale' and 'orientation' are nx1 vectors indicating the scale and
+%   orientation of each interest point. These are OPTIONAL. By default you
+%   do not need to make scale and orientation invariant local features.
+function [x, y, confidence, scale, orientation] = get_interest_points(image, feature_width)
+
+    WINDOW_SIZE = 5;
+
+    % Calculate the vertical and horizontal derivative
+    [Ix, Iy] = gradient(image);
+    
+    Ix_squared = Ix ^ 2;
+    IxIy = Ix * Iy;
+    Iy_squared = Iy ^ 2;
+    
+    windowed_Ix_squared = imgaussfilt(Ix_squared, WINDOW_SIZE);
+    windowed_IxIy = imgaussfilt(IxIy, WINDOW_SIZE);
+    windowed_Iy_squared = imgaussfilt(Iy_squared, WINDOW_SIZE);
+    
+    M = [windowed_Ix_squared  windowed_IxIy;
+         windowed_IxIy  windowed_Iy_squared;];
+     
+    eigenvalues = eig(M);
+
+end
+
