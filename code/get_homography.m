ab51d94b4ec81eda5c4609883d80fae28f4068a8
Peter Tan <pj.t@outlook.com>
2018-07-30 19:53:21 -0700

    Changes at 7:52PM 2018-07-30

--- code/get_homography.m
+++ code/get_homography.m
@@ -2,16 +2,25 @@ function homography = get_homography(matches, x1, y1, x2, y2)
 
     [n, ~] = size(matches);
     
-    numTrial = 1000;
+    % Filter out x1y1, x2y2 in matches array
+    x1_matches = zeros(n, 1);
+    y1_matches = zeros(n, 1);
+    x2_matches = zeros(n, 1);
+    y2_matches = zeros(n, 1);
+    
+    for i = 1:n
+        x1y1_ind = matches(i, 1);
+        x2y2_ind = matches(i, 2);
+        x1_matches(i) = x1(x1y1_ind); y1_matches(i) = y1(x1y1_ind);
+        x2_matches(i) = x2(x2y2_ind); y2_matches(i) = y2(x2y2_ind);
+    end
+    
+    numTrial = 10000;
     numSelectedPts = 4;
-    inlier_epsilon = 16;
+    inlier_epsilon = 30;
     
     num_inliers_best = 0;
     inliers_ind = [];
-    
-    [nx1, ~] = size(x1);
-    [nx2, ~] = size(x2);
-    min_x1x2 = min(nx1, nx2);
     
     
     for trial = 1:numTrial
@@ -24,17 +33,16 @@ function homography = get_homography(matches, x1, y1, x2, y2)
             
         h = computeHomography(indArr, x1, y1, x2, y2);
         
+        
         % Transform features1 using the homography computed
-        transformed_x1y1 = [x1 y1 ones(numel(x1), 1)] * h';
+        transformed_x1y1 = [x1_matches y1_matches ones(n, 1)] * h';
 
         % scale coordinates: making the z component to become 1
-        transformed_x1y1 = transformed_x1y1(1:min_x1x2, :);
         z = transformed_x1y1(:, end);
         transformed_x1y1 = bsxfun(@rdivide, transformed_x1y1, z);
         transformed_x1y1 = transformed_x1y1(:, 1:2);
 
-        x2y2 = [x2 y2];
-        x2y2 = x2y2(1:min_x1x2, :);
+        x2y2 = [x2_matches y2_matches];
         ssd_result = ssd(transformed_x1y1, x2y2);
         num_inliers = sum(ssd_result < inlier_epsilon);
         
@@ -44,6 +52,7 @@ function homography = get_homography(matches, x1, y1, x2, y2)
             inliers_ind = find(ssd_result < inlier_epsilon);
         end
     end
+    
     
     % Recompute homography for the greatest inliers
     homography = computeHomography([inliers_ind inliers_ind], x1, y1, x2, y2);
@@ -67,9 +76,6 @@ function h = computeHomography(indexArr, x1, y1, x2, y2)
     [~, ~, V] = svd(A);
     h = V(:, end);
     h = transpose(reshape(h, [3 3]));
-    
-    % Scale the homography so that the last element is 1
-%     h = h ./ h(end, end);
 end
 
 
