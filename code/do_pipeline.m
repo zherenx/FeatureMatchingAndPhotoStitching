084c55b55f1db1ee88998d71a7ded21d2a504fa9
Peter Tan <pj.t@outlook.com>
2018-07-31 21:34:28 -0700

    Changes at 9:34PM 2018-07-31

---
+++ code/do_pipeline.m
@@ -0,0 +1,17 @@
+function [im] = do_pipeline(im_ding, im_dong)
+    feature_width = 16; %width and height of each local feature, in pixels.
+
+    im_ding_bw = rgb2gray(im_ding);
+    im_dong_bw = rgb2gray(im_dong);
+    
+    [x1, y1] = get_interest_points_modified(im_ding_bw, feature_width);
+    [x2, y2] = get_interest_points_modified(im_dong_bw, feature_width);
+    
+    [im_ding_features] = get_features(im_ding_bw, x1, y1, feature_width);
+    [im_dong_features] = get_features(im_dong_bw, x2, y2, feature_width);
+    
+    [matches, confidences] = match_features(im_dong_features, im_ding_features);
+    homography = get_homography(matches, x2, y2, x1, y1);
+    
+    im = stitch_images(im_ding, im_dong, homography);
+end
